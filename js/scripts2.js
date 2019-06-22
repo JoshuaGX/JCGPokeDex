eab915b3c85e0873599c9a21fb10a90f92e44dc3
JoshuaGX <47896703+JoshuaGX@users.noreply.github.com>
2019-06-22 02:53:21 -0600

    Basic Formula
    Listed pokemon and attempted to try and get this to work without success

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,21 +1,45 @@
 var repository = [
-{
-  name: "Charmander",
-  type: "Fire",
-  height: "0.6m"
-},
-{
-  name: "Poliwrath",
-  type: ['Water', 'Fighting'],
-  height: "1.3m"
-},
-{
-  name: "Gengar",
-  type: ['Ghost', 'Poison'],
-  height: "1.5m"
-}
-];
+  ['Charmander',
+  0.6,
+  ['Fire']],
 
-for (var i = 0; i <=2; i++) {
-  document.write(repository)
-};
+  ['Poliwrath',
+  1.3,
+  ['Water', 'Fighting']],
+
+  ['Gengar',
+  1.5,
+  ['Ghost', 'Poison']],
+]
+
+
+console.log (repository[0],repository[1],repository[2]);
+
+
+for (var i = 0; i < repository.length; i++) {
+  if (repository[i][1] > 1.4) {
+    document.write ('<p class="top_article">Wow - he is the biggest Pokemon</p>');
+  }
+}
+  document.write ('<h2><a href="">' + repository[i],[0] + '</a></h2>');
+
+  document.write ('<p class="p_small">Types: ');
+  for (var j = 0; j < repository[i],[2].length; j++) {
+    if (repository[i],[2],[j] == 'Fire') {
+      document.write (' <span class="p_small">' + repository[i],[2],[j] + ',');
+    }
+    else if (repository[i],[2],[j] == 'Poison') {
+      document.write (' <span class="p_small"' + repository[i],[2],[j] + ',');
+    }
+    else if (repository[i],[2],[j] == 'Ghost') {
+      document.write (' <span class="p_small"' + repository[i],[2],[j] + ',');
+    }
+    else if (repository[i],[2],[j] == 'Water') {
+      document.write (' <span class="p_small"' + repository[i],[2],[j] + ',');
+    }
+    else if (repository[i],[2],[j] == 'Fighting') {
+      document.write (' <span class="p_small"' + repository[i],[2],[j] + ',');
+    }
+  }
+  document.write ('</p>');
+  document.write ('<br><br>');
