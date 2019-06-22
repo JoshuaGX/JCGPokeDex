73ea5ed7a8b8a8b5026bff37df0cf2f5ba456dc4
JoshuaGX <47896703+JoshuaGX@users.noreply.github.com>
2019-06-22 01:02:15 -0600

    first draft
    really just trying to get my bearings

---
+++ js/scripts2.js
@@ -0,0 +1,24 @@
+var repository = [
+{
+
+  name: "Charmander",
+  type: "Fire",
+  height: "0.6m"
+},
+{
+
+  name: "Poliwrath",
+  type: ['Water', 'Fighting'],
+  height: "1.3m"
+},
+{
+
+  name: "Gengar",
+  type: ['Ghost', 'Poison'],
+  height: "1.5m"
+}
+];
+
+for (var i = 0; i <=2; i++) {
+  document.write(repository)
+};
