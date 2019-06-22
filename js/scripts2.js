303cd340883b88e212d0e3f8a78875f518bc13cc
JoshuaGX <47896703+JoshuaGX@users.noreply.github.com>
2019-06-22 01:03:13 -0600

    Update scripts2.js
    working on the code separately as I have the instructions on the original page

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,18 +1,15 @@
 var repository = [
 {
-
   name: "Charmander",
   type: "Fire",
   height: "0.6m"
 },
 {
-
   name: "Poliwrath",
   type: ['Water', 'Fighting'],
   height: "1.3m"
 },
 {
-
   name: "Gengar",
   type: ['Ghost', 'Poison'],
   height: "1.5m"
