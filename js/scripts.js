559948896efc8de3a1a6ede5eee583372132df26
JoshuaGX <47896703+JoshuaGX@users.noreply.github.com>
2019-06-22 00:53:31 -0600

    first draft
    really just trying to get my bearings

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+
+//    Create a new variable called repository and assign it to be an array. This array will become the repository of Pokémon to display in your application.
+var pokeRepository = (function () {
+  var repository = [];
+//    Now, add several objects to the array. Each object represents one Pokémon and needs to have the same keys to avoid errors. Add at least the following three keys to each Pokémon object and make up a value for each of them: name (make this a string, e.g., "Bulbasaur"), height (this will be a number, e.g., 7) and types (make this an array of strings, e.g., ['grass', 'poison']). You can use Pokedex.org as a reference. Create at least three Pokémon objects in your repository array.
+
+//    Now that the repository is built, let’s output all your Pokémon. Below the repository in your “scripts.js” file, create a for loop. Iterate over each item of the repository and...
+//        Use document.write() inside the loop’s code to write the Pokémon name on your website’s DOM.
+//        Use what you’ve learned about adding strings in JavaScript to write the Pokémon’s height next to its name, for example, “Bulbasaur (height: 7)”.
+
+//    You should now be able to open the “index.html” of your project in the browser and see a list of Pokémon appear. Work with the inspector and JavaScript console of your Developer Tools to see if there are any errors in your code. You can also use the Web Inspector to adjust your CSS and make things look nicer.
+
+//    In this step, you'll highlight special Pokémon from your list. Within the loop, add a conditional. The conditional should check if the height is above a certain value. If it is, add the note “Wow, that’s big!” to the output. Make sure you set up the conditional so that only one Pokémon has the label “Wow, that’s big!” It could, for example, look like this: “Bulbasaur (height: 7) - Wow, that’s big!”
+
+//    Submit the link to your GitHub repository here. Feel free to share additional thoughts or ask questions on your submissions page.
